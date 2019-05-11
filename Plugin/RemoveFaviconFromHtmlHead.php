9000d30324cc2e919558c0e01e9823902d99518b
Mark Shust <mark@shust.com>
2019-05-10 21:02:17 -0400

    Added missing docblock (Magento Coding Standard)

--- Plugin/RemoveFaviconFromHtmlHead.php
+++ Plugin/RemoveFaviconFromHtmlHead.php
@@ -3,6 +3,10 @@ namespace MarkShust\RemoveFavicon\Plugin;
 
 use Magento\Framework\View\Page\Config\Renderer;
 
+/**
+ * Class RemoveFaviconFromHtmlHead
+ * @package MarkShust\RemoveFavicon\Plugin
+ */
 class RemoveFaviconFromHtmlHead
 {
     public function aroundPrepareFavicon(Renderer $subject, callable $proceed)
